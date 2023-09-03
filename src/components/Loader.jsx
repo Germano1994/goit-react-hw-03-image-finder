f6b7ad38314a2ae6e63b25e4d90cf3339c9877db
Germano1994 <125311105+Germano1994@users.noreply.github.com>
2023-09-03 22:33:05 +0300

    fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { TailSpin } from 'react-loader-spinner';
 
-const LoadingSpinner = () => {
+const Loader = () => {
   return (
     <div className="loader">
-      <TailSpin type="TailSpin" color="#00BFFF" height={80} width={80} />
+      <TailSpin color="#00BFFF" height={80} width={80} />
     </div>
   );
 };
 
-export default LoadingSpinner;
+export default Loader;
+
